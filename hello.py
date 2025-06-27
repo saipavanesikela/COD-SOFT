fff9e2d412bcce86b334bf86b205819c51f99c5a
saipavanesikela <esikelasaipavan9@gmail.com>
2025-06-27 06:58:30 +0530

    aded

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("hello")
+n=int(input("enter the number upto which you want the sum"))
+result=[]
+
+for i in range(2,n):
+    prime=True
+    for j in range(2,i):
+        if i%j==0:
+            prime=False
+    if prime:
+        result.append(i)
+
+print(sum(result))
